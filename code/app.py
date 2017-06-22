175fd75b41fef462dd0a4caf7d1bfebf57a8b5ec
Andy Mepham <andymeps@yahoo.com>
2017-06-22 14:05:47 +0100

    Finished section 4, final results

--- code/app.py
+++ code/app.py
@@ -1,24 +1,57 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'c05051f8-0e6d-4875-afcb-54c7b96ef03f'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 class Item(Resource):
-    def get(self, name):
-        item = filter(lambda x: x['name'] == name, items)
-        #for item in items:
-         #   if item['name'] == name:
-          #      return item
-        return {'item': None}, 404
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank!")
 
+    @jwt_required()
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    @jwt_required()
     def post(self, name):
-        data = request.get_json()
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': 'An item with name `{}` already exists.'.format(name)}, 400
+
+        data = Item.parser.parse_args()
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    @jwt_required()
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    @jwt_required()
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 
 class ItemList(Resource):
