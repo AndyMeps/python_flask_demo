0493d498a3e5db9c64a0ffd0b1b35f96c1173064
Andy Mepham <andymeps@yahoo.com>
2017-06-25 15:45:48 +0100

    Section 5 complete

--- code/security.py
+++ code/security.py
@@ -2,17 +2,9 @@
 from user import User
 from werkzeug.security import safe_str_cmp
 
-USERS = [
-    User(1, 'bob', 'asdf')
-]
-
-USERNAME_MAPPING = {u.username: u for u in USERS}
-
-USERID_MAPPING = {u.id: u for u in USERS}
-
 def authenticate(username, password):
     """ Auth """
-    user = USERNAME_MAPPING.get(username, None)
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
@@ -20,4 +12,4 @@ def authenticate(username, password):
 def identity(payload):
     """ Identity """
     user_id = payload['identity']
-    return USERID_MAPPING.get(user_id, None)
+    return User.find_by_id(user_id)
